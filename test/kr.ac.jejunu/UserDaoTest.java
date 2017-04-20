fe56ca4c57fa70c8706a40c5bbfa1aa0cf6de64a
replaykim <replaykim123@gmail.com>
2017-04-20 09:45:43 +0900

    1

--- test/kr.ac.jejunu/UserDaoTest.java
+++ test/kr.ac.jejunu/UserDaoTest.java
@@ -29,6 +29,25 @@ public class UserDaoTest {
         assertThat(id, is(user.getId()));
         assertThat(name, is(user.getName()));
         assertThat(password, is(user.getPassword()));
+    }
 
+    @Test
+    public void add() throws SQLException, ClassNotFoundException {
+        Long id = 20l;
+        String name  = "우아아악";
+        String password = "1234";
+
+        UserDao userDao = new UserDao();
+        User user = new User();
+        user.setId(id);
+        user.setName(name);
+        user.setPassword(password);
+        userDao.add(user);
+
+        User resultUser = userDao.get(id);
+
+        assertThat(id,is(resultUser.getId()));
+        assertThat(name, is(resultUser.getName()));
+        assertThat(password, is(resultUser.getPassword()));
     }
 }
